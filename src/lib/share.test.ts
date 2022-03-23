fd52705798bc3747ce3bf48d97d57a39c6e45a03
Alejo Zárate <alejo.pzarate@gmail.com>
2022-03-23 10:52:46 -0300

    [Fix] test generateEmojiGrid failing grid

--- src/lib/share.test.ts
+++ src/lib/share.test.ts
@@ -1,6 +1,5 @@
-import { generateEmojiGrid } from './share'
-
 const mockSolutionGetter = jest.fn()
+
 jest.mock('./words', () => ({
   ...jest.requireActual('./words'),
   get solution() {
@@ -9,26 +8,28 @@ jest.mock('./words', () => ({
 }))
 
 describe('generateEmojiGrid', () => {
-  test('generates grid for ascii', () => {
+  test('generates grid for ascii', async () => {
     const guesses = ['EDCBA', 'VWXYZ', 'ABCDE']
     const tiles = ['C', 'P', 'A'] // Correct, Present, Absemt
     mockSolutionGetter.mockReturnValue('ABCDE')
 
-    const grid = generateEmojiGrid(guesses, tiles)
+    const grid = (await import('./share')).generateEmojiGrid(guesses, tiles)
     const gridParts = grid.split('\n')
     expect(gridParts[0]).toBe('PPCPP')
     expect(gridParts[1]).toBe('AAAAA')
     expect(gridParts[2]).toBe('CCCCC')
   })
-  test('generates grid for ascii', () => {
+  test('generates grid for ascii', async () => {
     const guesses = ['5️⃣4️⃣3️⃣2️⃣1️⃣', '♠️♥️♦️♣️🔔', '1️⃣2️⃣3️⃣4️⃣5️⃣']
     const tiles = ['C', 'P', 'A'] // Correct, Present, Absemt
     mockSolutionGetter.mockReturnValue('1️⃣2️⃣3️⃣4️⃣5️⃣')
 
-    const grid = generateEmojiGrid(guesses, tiles)
+    const grid = (await import('./share')).generateEmojiGrid(guesses, tiles)
     const gridParts = grid.split('\n')
     expect(gridParts[0]).toBe('PPCPP')
     expect(gridParts[1]).toBe('AAAAA')
     expect(gridParts[2]).toBe('CCCCC')
   })
 })
+
+export {}
