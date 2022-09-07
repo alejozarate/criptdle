e41e6903e923a61536e148c6ba608f73ffed2136
Alejo Zárate <alejo.pzarate@gmail.com>
2022-09-07 17:14:08 -0300

    [Fix] add word

--- src/constants/worddescription.ts
+++ src/constants/worddescription.ts
@@ -218,4 +218,5 @@ export const WORDS_DESCRIPTION: StringMap = {
         'Comisión que se le paga los creadores de NFTs cada vez que sus obras se revenden. (Depende del marketplace) --- Definición de franquen',
     ripio: 'Exchange Centralizado Argentino, fue fundada en 2013, está creando su blockchain y tiene su token --- Definición de @marcoflorit',
     ziliqa: 'Ziliqa es una blockchain con un mecanismo de consenso que combina proof-of-work y pBFT al mismo tiempo que aplica sharding al procesamiento de la información. Permite contratos inteligentes escritos en un lenguaje de programación propio llamado Scilla. -- Definición de @franquen',
+    reward: 'En español, recompensa. Hace referencia a aquellos tokens o criptomonedas que podemos obtener como premio o retribución por realizar determinadas acciones en protocolos, wallets o blockchain --- Definición de @0xPumbi',
 }
